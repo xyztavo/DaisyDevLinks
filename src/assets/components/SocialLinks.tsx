8e5afe224f37b1e5dc7588e239af7f2446670469
ustav <gustavobreasy@gmail.com>
2024-01-14 23:14:06 -0300

    add animation to social link icons

--- src/assets/components/SocialLinks.tsx
+++ src/assets/components/SocialLinks.tsx
@@ -1,20 +1,65 @@
 import { FaDiscord, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 interface SocialLinksProps {
-    githubLink: string;
-    instagramLink: string;
-    discordLink: string; 
-    linkedinLink: string;
+  githubLink: string;
+  instagramLink: string;
+  discordLink: string;
+  linkedinLink: string;
 }
 const SocialLinks = (props: SocialLinksProps) => {
   return (
     <div className="flex flex-row justify-center mt-9">
-        <a href={props.githubLink} target="_blank" className="p-5 text-2xl"><FaGithub /></a>
-        <a href={props.instagramLink} target="_blank" className="p-5 text-2xl"><FaInstagram /></a>
-        <a href={props.discordLink} target="_blank" className="p-5 text-2xl"><FaDiscord /></a>
-        <a href={props.linkedinLink} target="_blank" className="p-5 text-2xl"><FaLinkedin /></a>
+      <motion.a
+        animate={{ opacity: 1 }}
+        initial={{
+          opacity: 0,
+        }}
+        whileHover={{ scale: 2 }}
+        href={props.githubLink}
+        target="_blank"
+        className="p-5 text-2xl"
+      >
+        <FaGithub />
+      </motion.a>
+      <motion.a
+        animate={{ opacity: 1 }}
+        initial={{
+          opacity: 0,
+        }}
+        whileHover={{ scale: 2 }}
+        href={props.instagramLink}
+        target="_blank"
+        className="p-5 text-2xl"
+      >
+        <FaInstagram />
+      </motion.a>
+      <motion.a
+        animate={{ opacity: 1 }}
+        initial={{
+          opacity: 0,
+        }}
+        whileHover={{ scale: 2 }}
+        href={props.discordLink}
+        target="_blank"
+        className="p-5 text-2xl"
+      >
+        <FaDiscord />
+      </motion.a>
+      <motion.a
+        animate={{ opacity: 1 }}
+        initial={{
+          opacity: 0,
+        }}
+        whileHover={{ scale: 2 }}
+        href={props.linkedinLink}
+        target="_blank"
+        className="p-5 text-2xl"
+      >
+        <FaLinkedin />
+      </motion.a>
     </div>
-  )
-}
+  );
+};
 
-export default SocialLinks
+export default SocialLinks;
